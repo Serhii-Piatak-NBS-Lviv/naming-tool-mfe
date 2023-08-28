12ec8050071c4a5864b09b34269e8031f4406318
Nazarii Lysak <Nazarii.Lysak@ua.nestle.com>
2023-08-28 11:21:13 +0300

    fix cebab-case styles

--- src/themes.js
+++ src/themes.js
@@ -509,12 +509,12 @@ export const themes = {
             "textAlign": 'center',
 
             '&.hidden': {
-                // "-webkitAnimationDuration": '3s',
                 "animationDuration": '3s',
-                // "-webkitAnimationFillMode": 'both',
                 "animationFillMode": 'both',
-                // "-webkitAnimationName": 'fadeOut',
                 "animationName": fadeOut,
+                "WebkitAnimationDuration": '3s',
+                "WebkitAnimationFillMode": 'both',
+                "WebkitAnimationName": fadeOut,
             },
         },
 
