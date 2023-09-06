dee8453ce810708723a5f230cce6b79013fd905a
Nazarii Lysak <Nazarii.Lysak@ua.nestle.com>
2023-09-06 13:14:57 +0300

    add logic for disable and enable filters and letters

--- src/features/filter/AlphabetSelector.jsx
+++ src/features/filter/AlphabetSelector.jsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import useThemifiedComponent from '../../app/hooks/useThemifiedComponent';
-import { cx } from '@emotion/css';
+import { cx, css } from '@emotion/css';
 import { useSelector } from 'react-redux';
 
 /**
@@ -10,6 +10,7 @@ import { useSelector } from 'react-redux';
 
 export const AlphabetSelector = ({handleLetter}) => {
   const activeLetter = useSelector(state => state.filter.letter);
+  const namesList = useSelector(state => state.view.names_list_full);
 
   const [cssAlphabeticalContainer] = useThemifiedComponent('filter-alphabetical-container');
   const [cssAlphabeticalList] = useThemifiedComponent('filter-alphabetical-list');
@@ -23,6 +24,20 @@ export const AlphabetSelector = ({handleLetter}) => {
     if(activeFilters.includes(title)) return cssLetterActive;
   }
 
+  const missedLetter = css`
+    opacity: 0.4;
+    pointer-events: none;
+  `
+
+  const checkLetter = (letter) => {
+    const isActive = activeLetters.includes(letter);
+    return !isActive ? missedLetter : null 
+  }
+
+  const activeLetters = Array.from(new Set([...namesList
+    .map(element => element.Title[0])
+    .filter(element => element !== " ")]));  
+
   return(
     <div className={cssAlphabeticalContainer}>
       <ul className={cssAlphabeticalList}>
@@ -30,7 +45,7 @@ export const AlphabetSelector = ({handleLetter}) => {
           alphabet.map(letter => {
             return (        
               <li 
-                className={cx(cssAlphabeticalletter, isActive(activeLetter, letter))} 
+                className={cx(cssAlphabeticalletter, isActive(activeLetter, letter), checkLetter(letter))} 
                 onClick={() => handleLetter(letter)}
                 key={letter}
               >
--- src/features/filter/SwiperCategoryItem.jsx
+++ src/features/filter/SwiperCategoryItem.jsx
@@ -1,10 +1,11 @@
 import useThemifiedComponent from '../../app/hooks/useThemifiedComponent';
 
 import { useSelector } from 'react-redux';
-import { cx } from '@emotion/css';
+import { cx, css } from '@emotion/css';
 
 const SwiperCategoryItem = ({category, handleFilter}) => {
     const activeFilters = useSelector(state => state.filter.selectedCategories);    
+    const namesList = useSelector(state => state.view.names_list_full);
 
     const [cssCategoryWrapper] = useThemifiedComponent('filter-category');
     const [cssCategoryIsActive] = useThemifiedComponent('isActive');
@@ -22,9 +23,26 @@ const SwiperCategoryItem = ({category, handleFilter}) => {
         return require(`../../app/images/${device}/${window.innerWidth > 768 ? icon_desktop_tablet : icon_mobile}`);
     };
 
+    const missedCategory = css`
+        opacity: 0.4;
+        pointer-events: none;
+    `
+
+    const activeCategories = Array.from(new Set([...namesList
+        .map(categories => categories.categories)
+        .filter(arr => arr.length > 0)
+        .reduce((flat, item) => {
+            return flat.concat(Array.isArray(item) ? item : [item]);
+        }, [])]));    
+
+    const checkCategory = (category) => {
+        const isActive = activeCategories.includes(category);
+        return !isActive ? missedCategory : null 
+    }
+
     return (
         <div        
-            className={cx(cssCategoryWrapper, isActive(activeFilters, category.id))}
+            className={cx(cssCategoryWrapper, isActive(activeFilters, category.id), checkCategory(category.id))}
             onClick={() => handleFilter(category.id)}
         >
             <img 
