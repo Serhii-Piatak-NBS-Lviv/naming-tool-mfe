09fbcf7364bf4281f32676356d9fc552333ee056
Nazarii Lysak <Nazarii.Lysak@ua.nestle.com>
2024-01-12 16:38:31 +0200

    fix grid (buttons)

--- src/features/view/View.jsx
+++ src/features/view/View.jsx
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { SimpleGrid } from '@chakra-ui/react';
@@ -10,26 +10,61 @@ import ShiftingPopover from './ShiftingPopover';
 **/
 
 export const View = () => {
-    const { t } = useTranslation();
-    const simpleGridRef = useRef();
-    const namesList = useSelector(state => state.view.names_list);
 
-    return(
-        <SimpleGrid ref={simpleGridRef} position={'relative'} minChildWidth='255px' spacing='16px' w={'100%'} mx="auto" maxW={1109}>
-            { namesList.map((petname) => {
-            return (
-              <ShiftingPopover
-              id={petname.id} 
-              title={petname.title} 
-              description={petname.definition}
-              gender={petname.gender}
-              categories={petname.categories}
-              simpleGridRef={simpleGridRef}
-              key={petname.id} />              
-            )}
-          ) }
-        </SimpleGrid>
-    )
+  const { t } = useTranslation();
+  const simpleGridRef = useRef();
+  const namesList = useSelector(state => state.view.names_list);
+  const [innerWidth, setInnerWidth] = useState(() => window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setInnerWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const defineGridColumns = () => {
+    switch (true) {
+      case innerWidth < 600:
+        return 1;
+      case innerWidth < 900:
+        return 2;
+      case innerWidth < 1139:
+        return 3;
+      default:
+        return 4;
+    }
+  };
+
+  return(
+    <SimpleGrid 
+      ref={simpleGridRef} 
+      position={'relative'} 
+      spacing='16px' 
+      w={'100%'} 
+      mx="auto" 
+      maxW={1109} 
+      columns={defineGridColumns()}
+    >
+      { namesList.map((petname) => {
+        return (
+          <ShiftingPopover
+          id={petname.id} 
+          title={petname.title} 
+          description={petname.definition}
+          gender={petname.gender}
+          categories={petname.categories}
+          simpleGridRef={simpleGridRef}
+          key={petname.id} />              
+        )}
+      )}
+    </SimpleGrid>
+  )
 };
 
 export default View;
